f42b87e517d5cbc720c28ed92cb0198d3fa669fb
Bruna Mello <brunacrespomello@gmail.com>
2023-03-27 14:52:13 +0100

    04. Mock
    Shall we replay what we learn? In this class, we added actions in our test and we produce a Mock with Stub. In addition, we understand more clearly the relations of test dependencies:
    
    Actions:
    When we test our applications, we are faced with actions of interaction with buttons, text boxes, scrollables, among others. Actions are instructions that we give to our tester to interact with certain widgets in order to verify their usability.
    
    Using mocks:
    We learned that Mock is used to create a double of external dependencies ( like API’s or databases outside the ) project and that it prevents us from having flaky tests.
    
    Operate with Stub:
    We found that it is not enough to just create a stuntman, we need this stuntman, in some cases, to give us a return of usable value, and the Stub is made to return this value in the name of the Stuntman.
    
    Understand test dependencies
    Now that we have started to go deeper into the tests, we can more easily see how complicated the dependencies between our Classes, methods and Widgets can be, especially in automated tests.
    
    Congratulations! You finished another Flutter course! Now you are one step closer to becoming a professional ( a ) in application production!
    
    Tell us what you think of this course, your feedback is extremely important so that the quality of Flutter courses is always charming.

--- test/home_test.dart
+++ test/home_test.dart
@@ -1,44 +1,72 @@
 import 'package:estilizacao_componentes/components/box_card.dart';
+import 'package:estilizacao_componentes/data/bank_http.mocks.dart';
 import 'package:estilizacao_componentes/data/bank_inherited.dart';
 import 'package:estilizacao_componentes/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 void main() {
+
+  late MockBankHttp httpMock;
+
+  setUp(() {
+    httpMock = MockBankHttp();
+  });
+
   testWidgets('My widget has a text "Spent', (tester) async {
+    when(httpMock.dolarToReal()).thenAnswer((_) async => ('5'));
+
     await tester.pumpWidget(MaterialApp(
       home: BankInherited(
-        child: const Home(),
+        child: Home(
+          api: httpMock.dolarToReal(),
+        ),
       ),
     ));
+
     final spentFinder = find.text('Spent');
     expect(spentFinder, findsOneWidget);
   });
 
   testWidgets('Find a linear progressive indicator', (tester) async {
+    when(httpMock.dolarToReal())
+        .thenAnswer((_) async => ('LinearProgressIndicator'));
+
     await tester.pumpWidget(MaterialApp(
       home: BankInherited(
-        child: const Home(),
+        child: Home(
+          api: httpMock.dolarToReal(),
+        ),
       ),
     ));
+
     expect(find.byType(LinearProgressIndicator), findsOneWidget);
   });
 
   testWidgets('find a AccountStatus', (tester) async {
+    when(httpMock.dolarToReal()).thenAnswer((_) async => ('5'));
+
     await tester.pumpWidget(MaterialApp(
       home: BankInherited(
-        child: const Home(),
+        child: Home(
+          api: httpMock.dolarToReal(),
+        ),
       ),
     ));
+
     expect(find.byKey(const Key('testKey')), findsOneWidget);
   });
 
   testWidgets('find 5 BoxCards', (tester) async {
+    when(httpMock.dolarToReal()).thenAnswer((_) async => ('5'));
+
     await tester.pumpWidget(MaterialApp(
       home: BankInherited(
-        child: const Home(),
+        child: Home(api: httpMock.dolarToReal()),
       ),
     ));
+
     expect(find.byWidgetPredicate((widget) {
       if (widget is BoxCard) {
         return true;
@@ -49,9 +77,13 @@ void main() {
   });
 
   testWidgets('When tap deposit should up load earned in 10', (tester) async {
+    when(httpMock.dolarToReal()).thenAnswer((_) async => ('5'));
+
     await tester.pumpWidget(MaterialApp(
       home: BankInherited(
-        child: const Home(),
+        child: Home(
+          api: httpMock.dolarToReal(),
+        ),
       ),
     ));
 
@@ -62,5 +94,19 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(find.text('\$10.0'), findsOneWidget);
+  });
+
+  testWidgets('Testing MockHttp dolartoReal', (tester) async {
+    when(httpMock.dolarToReal()).thenAnswer((_) async => ('5'));
+
+    await tester.pumpWidget(MaterialApp(
+      home: BankInherited(
+        child: Home(
+          api: httpMock.dolarToReal(),
+        ),
+      ),
+    ));
+
+    verify(httpMock.dolarToReal()).called(1);
   });
 }
